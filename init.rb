bc674a6577d37112d5b4c242006d7f17d5366e69
Dennis <1625376+erSitzt@users.noreply.github.com>
2026-08-19 15:07:54 +0200

    chore: release 0.2.1 (#7)
    Bug-fix release: reordering inside a column nobody has dragged into, and two cards dropped into the same spot taking the same rank. Dates both CHANGELOG sections, bumps init.rb, adds docs/redmine_org/releases/0.2.1.textile and corrects the rank design note in description.textile.

--- init.rb
+++ init.rb
@@ -40,7 +40,7 @@ Redmine::Plugin.register :redmine_expert_agile do
   name 'Redmine expert Agile'
   author 'Dennis Buehring'
   description 'Agile boards, story points, sprints and charts for Redmine'
-  version '0.2.0'
+  version '0.2.1'
   requires_redmine :version_or_higher => '5.0'
   url 'https://github.com/expertZentrale/redmine_expert_agile'
 
